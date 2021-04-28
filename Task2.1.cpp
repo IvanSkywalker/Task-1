030e350d7ccf63dae85be2b35e3ca684cf8f51ad
IvanSkywalker <82510393+IvanSkywalker@users.noreply.github.com>
2021-04-28 14:00:57 +0300

    Add files via upload

---
+++ Task2.1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int year;
+cout<<"Molq vuvedete godina: ";
+cin>>year;
+if(year%4==0){
+cout<<"Godinata e visokosna";
+}
+else{
+cout<<"Godinata ne e visokosna";
+}
+
+return 0;
+}
